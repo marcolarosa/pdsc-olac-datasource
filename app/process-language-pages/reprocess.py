09b730c35ccc9043edc19032a006ac112bf4be8f
Marco La Rosa <m@lr.id.au>
2018-07-16 15:50:22 +1000

    pause every 500 loads to give db time to breathe

--- app/process-language-pages/reprocess.py
+++ app/process-language-pages/reprocess.py
@@ -6,6 +6,7 @@ import os
 import sys
 import re
 import tarfile
+import time
 import shutil
 import json
 import requests
@@ -42,19 +43,26 @@ if __name__ == "__main__":
 
     zipped_path = "{0}/{1}.tbz".format(args.data, args.date)
     if os.path.exists(zipped_path):
-        tar = tarfile.open(path, 'r:bz2')
+        log.info("Extracting {0}".format(zipped_path))
+        tar = tarfile.open(zipped_path, 'r:bz2')
         tar.extractall(args.data)
         tar.close()
 
     path = "{0}/{1}".format(args.data, args.date)
     service = "{0}/languages".format(args.service)
+    i = 0
     for file in os.listdir(path):
         with open("{0}/{1}".format(path, file)) as f:
+            i += 1
             log.info("Processing: {0}".format(file))
             data = json.loads(f.read(), encoding="utf-8")
             if 'date' not in data:
                 data['date'] = args.date
             headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
             response = requests.post(service, data=json.dumps(data), headers=headers);
+            if i == 500:
+                log.info("Processed 500. Sleeping for 5")
+                time.sleep(5)
+                i = 0
             if response.status_code != 200:
                 log.error("Problem submitting: {0}".format(file))
