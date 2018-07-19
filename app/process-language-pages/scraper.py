dde601ded9d7878c61af57fd44cd03b008ff6984
Marco La Rosa <m@lr.id.au>
2018-07-19 19:17:05 +1000

    set default values

--- app/process-language-pages/scraper.py
+++ app/process-language-pages/scraper.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create Summary Page')
 
     parser.add_argument('--languages', dest='languages',
-                        help='The path to the CSV file containing the language codes', required=True)
+                        help='The path to the CSV file containing the language codes', default="languages.csv")
     parser.add_argument('--glotto-languoids', dest='languoids',
-                        help='The path to glottolog languoids CSV file', required=True)
+                        help='The path to glottolog languoids CSV file', default="languoid.csv")
     parser.add_argument('--mode', dest='mode', choices=['development', 'testing'],
                         help='The mode in which to run - limits the processing.')
-    parser.add_argument('--service', dest='service', help='The url to submit the data to.', required=True)
+    parser.add_argument('--service', dest='service', help='The url to submit the data to.', default="http://localhost:3000")
     parser.add_argument('--output-folder', dest='output', help='The folder to write the data to.', required=True)
 
     parser.add_argument('--info', dest='info', action='store_true',
